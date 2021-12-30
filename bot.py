f7704c5f878cf39bc9bd9f485b7358cd32ddf9fb
alisalimi80 <96053924+alisalimi80@users.noreply.github.com>
2021-12-30 07:53:03 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,63 @@
-# -*- coding: utf-8 -*-
-import redis
-import os
+#!/usr/bin/python
+
+# This is a simple echo bot using the decorator mechanism.
+# It echoes any incoming text messages.
+from bs4 import BeautifulSoup
+import requests
 import telebot
-# import some_api_lib
-# import ...
 
-# Example of your code beginning
-#           Config vars
-token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
-#             ...
+API_TOKEN = '5051387618:AAEFB8Pb79B3UVNqmWaOZXVtLKUqqq4GSEI'
 
-# If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
-r = redis.from_url(os.environ.get("REDIS_URL"))
+bot = telebot.TeleBot(API_TOKEN)
 
-#       Your bot code below
-# bot = telebot.TeleBot(token)
-# some_api = some_api_lib.connect(some_api_token)
-#              ...
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+@bot.message_handler(commands=['seke'])
+def send_welcome(message):
+    url = "https://www.tgju.org/coin"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
+    pricelist=[]
+    pricelist1=[]
+    elem = soup.find(class_='market-details-price')
+    pricelist.append( elem.text.split(':')[1].replace("\n",""))
+
+    url = "https://www.tgju.org/"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
+
+    elem1 = soup.find_all('td',{'data-market-row','price_dollar_rl'})
+    bot.reply_to(message,pricelist[0])
+
+@bot.message_handler(commands=['dolar'])
+def send_welcome(message):
+    try:
+        url = "https://www.tgju.org/"
+        req = requests.get(url)
+        soup = BeautifulSoup(req.text, "html.parser")
+
+        elem1 = soup.find_all(class_='info-price')
+        dolar = elem1[5].text
+
+        bot.reply_to(message,dolar)
+    except:
+        bot.reply_to(message,"404 not found")
+        
+
+
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
